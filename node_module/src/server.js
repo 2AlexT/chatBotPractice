8415596cc949eee6549130dcf42fa3d665ee1aa8
Dilan Alexander Peredo Flores <hlifeocl@gmail.com>
2022-08-30 19:22:37 -0400

    make it better

--- node_module/src/server.js
+++ node_module/src/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 //init all web routes
-    initWebRoutes(app);
+    initWebRoutes(app)
 
 let port = process.env.PORT || 8080;
 
